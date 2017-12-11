076b48f3202e51d8610f1c43e31fbdedf4212fc2
IvKosar <kosarevych@ucu.ed.ua>
2017-12-11 18:01:26 +0200

    class idexator done, class graphInterface expanded

--- source/preindexing/graph_interface.py
+++ source/preindexing/graph_interface.py
@@ -3,6 +3,21 @@ class GraphInterface:
     Created to simplify manipulations with possibly unfamiliar graph
     """
     class Node:
+        class Neighbour:
+            def __init__(self, i, c):
+                self._index = i
+                self._cost = c
+
+            def __repr__(self):
+                return "Neighbour(index={0}, cost={1})".format(self._index, self._cost)
+
+            def get_index(self):
+                return self._index
+
+            def get_cost(self):
+                return self._cost
+
+        # Node code begins
         def __init__(self, i, fv, lst):
             """
 
@@ -12,7 +27,10 @@ class GraphInterface:
             """
             self._node_index = i
             self._feature_vector = fv
-            self._neighbours = lst
+            self._neighbours = self._create_neighbours(lst)
+
+        def __repr__(self):
+            return "Node({})".format(self._node_index)
 
         def get_index(self):
             return self._node_index
@@ -23,11 +41,27 @@ class GraphInterface:
         def get_neighbours(self):
             return self._neighbours
 
+        @staticmethod
+        def get_neighbour_index(neighbour):
+            return neighbour.get_index()
 
+        @staticmethod
+        def get_neighbour_cost(neighbour):
+            return neighbour.get_cost()
+
+        @staticmethod
+        def _create_neighbours(lst):
+            neighbours = []
+            for pair in lst:
+                neighbour = GraphInterface.Node.Neighbour(pair[0], pair[1])
+                neighbours.append(neighbour)
+            return neighbours
+
+    # GrapInterface code begins
     def __init__(self, feature_number):
         self.nodes = []
         self._feature_number = feature_number
-        # self._build_graph() TODO: write this function
+        self._build_graph(None) #TODO: write this function
 
     def get_nodes(self):
         for node in self.nodes:
@@ -48,12 +82,23 @@ class GraphInterface:
     def get_node_neighbours(node):
         return node.get_neighbours()
 
+    @staticmethod
+    def get_node_neighbour_index(neighbour):
+        return GraphInterface.Node.get_neighbour_index(neighbour)
+
+    @staticmethod
+    def get_node_neighbour_cost(neighbour):
+        return GraphInterface.Node.get_neighbour_cost(neighbour)
+
     def _build_graph(self, graph):
         """
-        #TODO: WRITE USE IGRAPH LIBRARY http://igraph.org/python/doc/igraph.GraphBase-class.html
+        #TODO: WRITE USING IGRAPH LIBRARY http://igraph.org/python/doc/igraph.GraphBase-class.html
         Build graph interface from graph from other library
         :param graph: some object of graph from foreign library
         :return:
         """
-        pass
+        node1 = GraphInterface.Node(0, (0.6, 0.8, 0.1), [(1, 5), (2, 4)])
+        node2 = GraphInterface.Node(1, (0.0, 1.0, 0.9), [(0, 5)])
+        node3 = GraphInterface.Node(2, (0.1, 0.3, 0.0), [(0, 4)])
+        self.nodes = [node1, node2, node3]
 
--- source/preindexing/indexator.py
+++ source/preindexing/indexator.py
@@ -2,17 +2,69 @@ from source.preindexing.graph_interface import GraphInterface
 from source.preindexing.feature_index import FeatureIndex
 from source.preindexing.hop_index import HopIndex
 
+
 class Indexator:
     """
     Creates feature and hop indices from given graph
     IN: GraphInterface object
     OUT: FIQ, HIQ, VQ
     """
-    def __init__(self, graph):
+
+    def __init__(self, graph, query):
         self.graph = graph
-        self.FI = FeatureIndex()
-        self.HI = HopIndex()
+        self.query = query
 
     def make_indexing(self):
+        """
+        :return: tuple: (VQ, FIQ, HIQ)
+        """
+        # Build feature index
+        FI = FeatureIndex(self.graph.get_feature_number())
         for node in self.graph.get_nodes():
-            pass
+            feature_vector = GraphInterface.get_node_feature_vector(node)
+            for h in range(len(feature_vector)):
+                if feature_vector[h] != 0.0:
+                    FI.add_element(h, (node.get_index(), feature_vector[h]))
+
+        # Find valid nodes for Hop Index (VQ)
+        start = self.query.get_start()
+        finish = self.query.get_finish()
+        user_preference = self.query.get_preference()
+        filtering_vector = self.query.get_teta()
+        VQ = FI.find_valid_nodes(start, finish, user_preference, filtering_vector)
+
+        # Retrieve FIQ
+        FIQ = FI.get_fi()
+
+        # Build Hop Index
+        HI = HopIndex(VQ)
+        used_nodes = set()
+        for node in self.graph.get_nodes():
+            if GraphInterface.get_node_index(node) in VQ:
+                current_node_index = GraphInterface.get_node_index(node)
+                HI.add_element(current_node_index, (current_node_index, 0))
+                for neighbour in GraphInterface.get_node_neighbours(node):
+                    neighbour_index = GraphInterface.get_node_neighbour_index(neighbour)
+                    if neighbour_index in VQ and neighbour_index not in used_nodes:
+                        cost_to_node = GraphInterface.get_node_neighbour_cost(neighbour)
+                        HI.add_element(GraphInterface.get_node_index(node),
+                                       (neighbour_index, cost_to_node))
+            used_nodes.add(GraphInterface.get_node_index(node))
+        # Retrieve Hop Index HIQ
+        budget = self.query.get_budget()
+        HIQ = HI.get_hi(budget)
+
+        return FIQ, HIQ, VQ
+
+
+if __name__ == "__main__":
+    from source.user_query import Query
+
+    FEATURE_NUMBER = 3
+    graph = GraphInterface(FEATURE_NUMBER)
+    query = Query(0, 2, 3, (0.5, 0.5, 0.5), (0.6, 0.6, 0.6))
+    indexator = Indexator(graph, query)
+    FIQ, HIQ, VQ = indexator.make_indexing()
+    print("FIQ:" , FIQ)
+    print("HIQ:", HIQ)
+    print("VQ:", VQ)
