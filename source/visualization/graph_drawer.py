58af650505b31f21b016a35da7eb08d6ad2a01d9
borsukvasyl <v.borsuk2000@gmail.com>
2017-12-14 12:40:56 +0200

    added graph visualization

--- source/visualization/graph_drawer.py
+++ source/visualization/graph_drawer.py
@@ -1,12 +1,35 @@
-from graph_tool.all import *
+import matplotlib.pyplot as plt
+import networkx as nx
 
+G=nx.Graph()
 
-g = Graph(directed=False)
-v1 = g.add_vertex()
-v2 = g.add_vertex()
-e = g.add_edge(v1, v2)
-graph_draw(g, vertex_text=g.vertex_index, output_size=(200, 200), output="1.png")
+G.add_edge('a','b',weight=0.6)
+G.add_edge('a','c',weight=0.2)
+G.add_edge('c','d',weight=0.1)
+G.add_edge('c','e',weight=0.7)
+G.add_edge('c','f',weight=0.9)
+G.add_edge('a','d',weight=0.3)
 
+elarge=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] >0.5]
+esmall=[(u,v) for (u,v,d) in G.edges(data=True) if d['weight'] <=0.5]
+
+pos=nx.spring_layout(G) # positions for all nodes
+
+# nodes
+nx.draw_networkx_nodes(G,pos,node_size=700)
+
+# edges
+nx.draw_networkx_edges(G,pos,edgelist=elarge,
+                    width=6)
+nx.draw_networkx_edges(G,pos,edgelist=esmall,
+                    width=6,alpha=0.5,edge_color='b',style='dashed')
+
+# labels
+nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
+
+plt.axis('off')
+plt.savefig("weighted_graph.png") # save as png
+plt.show() # display
 '''
 class GraphDrawer(object):
     @staticmethod
